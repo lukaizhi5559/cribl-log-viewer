ba96ba0d4080e5d4165ac0d19409e3586e34cdb7
Chris <lukaizhi@chriss-air.mynetworksettings.com>
2024-09-25 09:31:51 -0400

    feat: initial app setup and added Cribl logo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';  // For Jest DOM matchers
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders App with Cribl logo', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Log Viewer/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import CriblLogo from "./components/CriblLogo";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-grid">
+      <header className="header">
+        <CriblLogo />
+        <span>Log Viewer</span>
       </header>
     </div>
   );
-}
+};
 
 export default App;
